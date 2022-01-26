2908c1ff7e2b4d12423844ba54be4df109220a17
sahil arora <sahilarora@sahils-MacBook-Air.local>
2022-01-26 21:19:08 +0530

    pushchanges

--- booksite/booksite/settings.py
+++ booksite/booksite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlewareWhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'booksite.urls'
